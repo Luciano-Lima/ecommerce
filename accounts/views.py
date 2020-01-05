223dc59831c57a7e95368c941e6fa09b82b2c494
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-05 23:43:22 +0000

    Returning products from the database

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
+
+
 # Create your views here.
 def index(request):
     """Return index.html file"""
