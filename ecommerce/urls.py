19ad257fdd99f48b34761b64f30005f7c4ff1f3c
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-11 00:05:45 +0000

    Added search url to urlpatterns

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,9 +19,11 @@ from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as products_urls
 from cart import urls as cart_urls
+from search import urls as search_urls
 from products.views import returns_all_products
 from django.views import static
 from .settings import MEDIA_ROOT
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -29,5 +31,6 @@ urlpatterns = [
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
     url(r'^cart/', include(cart_urls)),
+    url(r'^search/', include(search_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
