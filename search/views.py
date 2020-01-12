ccff84f8ee669081cf0ef7c8ffc42b1ecc17a79a
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-12 00:15:33 +0000

    Fixed method GET error

--- search/views.py
+++ search/views.py
@@ -3,5 +3,6 @@ from products.models import Product
 
 # Create your views here.
 def search_products(request):
-    queryset = Product.objects.filter(name__icontains=request.Get['q'])
+    queryset = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": queryset})
+    
