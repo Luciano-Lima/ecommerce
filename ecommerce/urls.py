dd5557653dc1afd11202b816f6c67b486dd9281c
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-14 14:17:32 +0000

    Adding the checkout url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,6 +20,7 @@ from accounts import urls as accounts_urls
 from products import urls as products_urls
 from cart import urls as cart_urls
 from search import urls as search_urls
+from checkout import urls as checkout_urls
 from products.views import returns_all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -32,5 +33,6 @@ urlpatterns = [
     url(r'^products/', include(products_urls)),
     url(r'^cart/', include(cart_urls)),
     url(r'^search/', include(search_urls)),
+    url(r'^checkout/', include(checkout_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
