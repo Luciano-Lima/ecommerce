864fb3bfeaf97aa7e31391dc6889b73cf83c4e41
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-19 00:58:30 +0000

    fixed missing Meta class

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,15 +5,16 @@ from .models import Order
 # Checkout Payment form 
 class PaymentForm(forms.Form):
     MONTH = [(i, i) for i in range(1, 12)]
-    YEAR = [(i, i) for i in range(2020, 2025)]
+    YEAR = [(i, i) for i in range(2020, 2028)]
 
     cc_number = forms.CharField(label='Card Number', required=False)
     cc_cvv= forms.CharField(label='Cvv', required=False)
-    cc_exp_month = forms.Select(label='Expire Month', choices=MONTH, required=False)
-    cc_exp_year = forms.Select(label='Expire Year', choices=YEAR, required=False)
-    strip_id =  forms.CharField(widget=forms.HiddenInput)
+    cc_exp_month = forms.ChoiceField(label='Expire Month', choices=MONTH, required=False)
+    cc_exp_year = forms.ChoiceField(label='Expire Year', choices=YEAR, required=False)
+    stripe_id =  forms.CharField(widget=forms.HiddenInput)
 
 
 class OrderForm(forms.ModelForm):
-    model = Order
-    fields = ('first_name', 'last_name', 'address', 'city', 'post_code', 'email', 'country', 'date') 
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'address', 'city', 'post_code', 'email', 'country', 'date') 
