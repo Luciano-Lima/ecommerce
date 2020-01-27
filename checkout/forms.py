54cfc506e9bd3a1177e7af17c6dd1701d8661ea2
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-01-27 20:17:36 +0000

    Removed date from Meta class

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,14 +7,15 @@ class PaymentForm(forms.Form):
     MONTH = [(i, i) for i in range(1, 12)]
     YEAR = [(i, i) for i in range(2020, 2028)]
 
-    cc_number = forms.CharField(label='Card Number', required=False)
-    cc_cvv= forms.CharField(label='Cvv', required=False)
-    cc_exp_month = forms.ChoiceField(label='Expire Month', choices=MONTH, required=False)
-    cc_exp_year = forms.ChoiceField(label='Expire Year', choices=YEAR, required=False)
+    number = forms.CharField(label='Card Number', required=False)
+    cvv = forms.CharField(label='Security code (cvv)', required=False)
+    exp_month = forms.ChoiceField(label='Expiry Month', choices=MONTH, required=False)
+    exp_year = forms.ChoiceField(label='Expiry Year', choices=YEAR, required=False)
     stripe_id =  forms.CharField(widget=forms.HiddenInput)
+    
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('first_name', 'last_name', 'address', 'city', 'post_code', 'email', 'country', 'date') 
+        fields = ('name', 'address', 'city', 'post_code', 'country') 
