e60c4573d69c3e84e797d1dab31220d7b702c0d2
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-08 00:16:43 +0000

    Created a Categorie Class

--- products/models.py
+++ products/models.py
@@ -1,11 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=200, unique=True) 
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+    
+    def __str__(self):
+        return self.name
+    
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=200, default='')
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200, unique=True)  
 
     def __str__(self):
         return self.name
