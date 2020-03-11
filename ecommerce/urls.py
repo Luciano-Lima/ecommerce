4e1ced1a216ac5c121945c9281cabf72fd3b11ab
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-11 10:32:56 +0000

    Removed duplicated url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,18 +21,21 @@ from products import urls as products_urls
 from cart import urls as cart_urls
 from search import urls as search_urls
 from checkout import urls as checkout_urls
-from products.views import home
+from products.views import index, categories
 from django.views import static
 from .settings import MEDIA_ROOT
 
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name="home"),
+    url(r'^$', index, name="index"),
+    url(r'^$', categories, name="categories"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
     url(r'^cart/', include(cart_urls)),
     url(r'^search/', include(search_urls)),
     url(r'^checkout/', include(checkout_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    
 ]
