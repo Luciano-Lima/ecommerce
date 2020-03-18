fa745c2dc7d21d89138a0b3a2c95bb5f18dc2670
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-18 10:46:19 +0000

    Fixed url Reverse error

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import index, categories
+from .views import index, categories, productDetail
 # from . import views
 from products.models import Product
 
@@ -8,5 +8,9 @@ from products.models import Product
 urlpatterns = [
     url(r'^$', index, name='home_page'),
     url(r'^(?P<category_slug>[-\w]+)/$', categories, name='products_by_category'),
+    url(r'^(?P<category_slug>\w+)/(?P<product_slug>[-\w]+)/$', productDetail, name='product_detail'),
     
+
+
 ]
+
