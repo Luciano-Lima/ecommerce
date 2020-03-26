6d65318a33bd0790262b88bac400b2a021a46ef1
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-26 14:01:43 +0000

    Added paginator

--- search/views.py
+++ search/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-# Create your views here.
+
 def search_products(request):
-    queryset = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "home.html", {"products": queryset})
+    products_items = Product.objects.all().order_by('name')
+    query = request.GET.get('q')
+    if query:
+        products_items = Product.objects.filter(name__icontains=query)
+    paginator = Paginator(products_items, 2)
+    page = request.GET.get('page')
+    try: 
+        product = paginator.page(page)
+    except PageNotAnInteger:
+        product = paginator.page(1) #returns the first page if page not integer
+    except EmptyPage:
+        product = paginator.page(paginator.num_pages) #returns last page if page out of range
     
+    return render(request, "home.html", {"products": product})
