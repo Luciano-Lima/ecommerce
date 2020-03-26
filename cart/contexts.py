9250bb85d165478c77ede8ef5839949ffc503572
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-26 14:21:16 +0000

    Displayina message

--- cart/contexts.py
+++ cart/contexts.py
@@ -14,6 +14,7 @@ def cart_products(request):
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
         number_of_items += quantity
+       
         cart_items.append({'id':id, 'quantity':quantity, 'product':product})
     return {'cart_items': cart_items, 'total': total, 'number_of_items': number_of_items}  
 
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from products.models import Product
 
 # Create your views here.
 def view_cart(request):
@@ -18,15 +19,16 @@ def add_to_cart(request, id):
     return redirect(reverse('index'))
 
 
-
-
 def update_cart(request, id):
     """Add or Remove itens from the cart"""
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart',{})
+    products = Product.objects.all()
     if quantity >= 0:
         cart[id] = quantity
     else:
         cart.pop(id)
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+    
+       
