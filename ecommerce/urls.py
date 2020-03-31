a4d4e48e3e2c26c894c02b400d4f48de64f871c7
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-03-31 20:25:50 +0100

    Replaced Accounts urls for Users

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts.views import index
-from accounts import urls as accounts_urls
+from users.views import index
+from users import urls as accounts_urls
 from products import urls as products_urls
 from cart import urls as cart_urls
 from search import urls as search_urls
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^$', categories, name="categories"),
-    url(r'^accounts/', include(accounts_urls)),
+    url(r'^users/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
     url(r'^cart/', include(cart_urls)),
     url(r'^search/', include(search_urls)),
