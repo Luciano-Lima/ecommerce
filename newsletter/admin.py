fe12c8950dde724731ba92b6abddc02879a80636
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-01 13:34:18 +0100

    Register Newsletter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Newsletter
 
+# Displays email and date_added on admin
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ('email', 'date_added',)
 
