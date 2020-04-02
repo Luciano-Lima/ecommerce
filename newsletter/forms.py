452c3f0b0a29c9f4defce1bb983c1871e273dcf4
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-02 22:29:03 +0100

    Creating newsletter form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,7 @@ class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        return email
