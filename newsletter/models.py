d1d96657b274626dd0b9080caa3cf80a925d74cd
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-03 03:27:59 +0100

    Removed testing widget

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 
 class Newsletter(models.Model):
     email = models.EmailField()
     date_added = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.email
+    
+
+
+    
