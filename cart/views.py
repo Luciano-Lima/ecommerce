2591caa7932b187028bfc8fc67636b98b2381c31
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-05 16:03:01 +0100

    Created delete_cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 def view_cart(request):
@@ -9,7 +10,10 @@ def view_cart(request):
 
 def add_to_cart(request, id):
     """Add items to cart"""
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity') or 0)
+    if quantity == 0:
+        messages.warning(request,'Please enter a quantity!')
+        
     cart = request.session.get('cart', {})
     if id in cart:
         cart[id] = int(cart[id]) + quantity      
@@ -17,18 +21,36 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity) 
     request.session['cart'] = cart
     return redirect(reverse('index'))
+        
+    
 
 
 def update_cart(request, id):
     """Add or Remove itens from the cart"""
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity')or 0)#prevents literal for int() error
+    if quantity == 0:  
+        messages.warning(request,'Enter a new Quantity or Proceed to Checkout!')
+        # return redirect(reverse('checkout'))
+    else:
+        cart = request.session.get('cart',{})
+        products = Product.objects.all()
+        if quantity >= 0:
+            cart[id] = quantity
+            messages.success(request,'Your cart was updated sucessfully!')
+        else:
+            cart.pop(id)
+        request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+  
+def delete_cart(request, id):
+    quantity = int(request.POST.get('quantity')or 0)
     cart = request.session.get('cart',{})
     products = Product.objects.all()
     if quantity >= 0:
-        cart[id] = quantity
-    else:
         cart.pop(id)
-    request.session['cart'] = cart
+        request.session['cart'] = cart
+        messages.success(request,'Item removed from your Cart!')
     return redirect(reverse('view_cart'))
     
-       
