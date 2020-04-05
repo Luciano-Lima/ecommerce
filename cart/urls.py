3c45015c2158884c982033c9dc8c95d555c55ca3
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-05 15:58:54 +0100

    Added delete_cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, update_cart
+from .views import view_cart, add_to_cart, update_cart, delete_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
     url(r'^update/(?P<id>\d+)', update_cart, name='update_cart'),
+    url(r'^delete/(?P<id>\d+)', delete_cart, name='delete_cart'),
 ]
