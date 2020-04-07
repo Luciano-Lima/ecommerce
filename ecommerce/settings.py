4c4cc8f9802314338b11b59c53f696e95fb7aadd
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-04-07 18:40:14 +0100

    Allowing host header

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,7 @@ STRIPE_SECRET = os.environ.get('STRIPE_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luciano-online-store.herokuapp.com']
 
 
 # Application definition
